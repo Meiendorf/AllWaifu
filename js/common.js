b8a1b39d11369eaebcb7dde019f26bd31035af5c
Meiendorf <neo1110111@gmail.com>
2017-11-23 22:29:20 +0200

    Fixes of waifu page

--- js/common.js
+++ js/common.js
@@ -1,7 +1,42 @@
 $(document).ready(function() {
+
+	comments = document.getElementById("wcomments");
+	comments.onkeydown = function(){
+		var el = this;
+		setTimeout(function(){
+			el.style.cssText = 'height:auto; padding:3px;';
+			el.style.cssText = 'height:' + el.scrollHeight+ 'px';
+		 },0);
+	}
 	$(".men_but").click(function(){
 		$(".desk_menu").slideToggle();
 	});
+	var i = 0;
+	var isPop = true;
+	$(".f_var").click(function(){
+		var remClass = "fa-heart-o"
+		var addClass = "fa-heart"
+		if (i == 1) {
+			remClass = [addClass, addClass=remClass][0]
+		}
+		$(this).removeClass(remClass);
+		$(this).addClass(addClass);
+		if (i == 1){
+			i = 0;
+		} else {
+			if (isPop){
+				popRemove("pop_show", "pop_hide");
+				isPop = false;
+				setTimeout(popRemove, 2000, "pop_hide", "pop_show");
+			}
+			i = 1;
+		}
+	});
+	function popRemove(add, remove){
+		$(".pop_div").removeClass(remove);
+		$(".pop_div").addClass(add);
+		isPop = true;
+	}
 	//Таймер обратного отсчета
 	//Документация: http://keith-wood.name/countdown.html
 	//<div class="countdown" date-time="2015-01-07"></div>
